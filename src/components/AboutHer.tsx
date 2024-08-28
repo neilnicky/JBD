bcfbed352c76be56611d387398bb88c6254d324d
Neil Nicholas <neilnicholaskanat@gmail.com>
2024-08-28 06:40:56 +0530

    first commit

--- src/components/AboutHer.tsx
+++ src/components/AboutHer.tsx
@@ -3,13 +3,17 @@ import React from "react";
 export default function AboutHer() {
   return (
     <div className="flex justify-center my-24">
-      <h2 className="text-lg md:text-2xl text-center font-medium w-[400px] md:w-[900px] ">
-        On your special day, I wish you all the happiness you truly deserve. May
+      <h2 className="text-lg md:text-2xl text-center font-medium w-[400px] md:w-[900px] text-slate-200">
+        On this special day, I wish you all the happiness you truly deserve. May
         your year ahead be filled with joy, laughter, and the courage to chase
-        your dreams. As you celebrate, may you be surrounded by love, warmth,
-        and all the beautiful moments life has to offer. Happy birthday to
-        someone who holds a special place in my heart—may life treat you as
-        kindly as you’ve treated those around you. You deserve nothing less than
+        your <span className="text-blue-500">dreams</span>. As you celebrate,
+        may you be surrounded by{" "}
+        <span className="text-yellow-500">
+          love, warmth, and all the beautiful moments
+        </span>{" "}
+        life has to offer. Happy birthday to someone who holds a special place
+        in my heart—may life treat you as kindly as you’ve treated those around
+        you. You <span className="text-red-500">deserve</span> nothing less than
         a lifetime of happiness, today and always.
       </h2>
     </div>
